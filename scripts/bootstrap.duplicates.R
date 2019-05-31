1c67717646db2a0bc09f8526afd8994da56421a0
LeeKennedy <mail@leekennedy.id.au>
2019-05-31 13:49:00 +1000

    Revamped bootstrap.duplicates.R

--- scripts/bootstrap.duplicates.R
+++ scripts/bootstrap.duplicates.R
@@ -1,22 +1,28 @@
-library(psych)
-library(dplyr)
-library(ggplot2)
+#### Clean Up environment -----------------------------
+rm(list=ls())
 
-# Enter data as csv file: Item/Result 1/Result 2 -------------------------
-data.in <- read.csv("Protein.csv", as.is=TRUE, header=TRUE)
-bucket <- rep(NA,1000)
+library(psych)
+library(readxl)
+library(tidyverse)
+library(here)
+
+# Enter data file: Item/Result 1/Result 2 -------------------------
+here::here()
+data.in <- read_excel("data/Book1.xlsx")
+data.in$diff <- data.in$A - data.in$B
+bucket <- rep(NA,10000)
 n=nrow(data.in)
 
+
 # Bootstrapping ----------------------------------------------------------
-for(i in 1:1000){
+for(i in 1:10000){
 
-samp <- sample(data.in$Item, n, replace=TRUE)
+set <- as.data.frame(sample(data.in$diff, n, replace=TRUE))
+colnames(set)[1] <- "Diff"
 
-set <- data.in[samp,]
 
 temp <- set %>%
-        mutate(Diff2 = (A-B)^2) %>%
-        summarise (std = sqrt(sum(Diff2)/(2*n)))
+        summarise (std = sqrt(sum(Diff)/(2*n)))
 
 bucket[i] = temp
 }
@@ -30,14 +36,22 @@ hist(bucket_2$Number, breaks = 20)
 summary(bucket_2$Number)
 describe(bucket_2$Number)
 
+bucket_2 <- na.omit(bucket_2)
+
 SEM <- sd(bucket_2$Number)/sqrt(n)
+
 SEM
 
 hranges <- quantile(bucket_2$Number, c(0.025, 0.975))
 hranges
 plot <- ggplot(bucket_2, aes(x=Number)) +
-        geom_histogram(binwidth = 0.005, colour="darkgreen", fill = "burlywood1") +
+        geom_histogram(binwidth = 0.03, colour="darkgreen", fill = "cornflowerblue") +
         geom_vline(xintercept = hranges[1], lty=2, colour = "red") +
         geom_vline(xintercept = hranges[2], lty=2, colour = "red") +
-        theme_bw()
+        labs(x = "Standard Deviation") +
+        theme_bw()+
+        theme(panel.grid.major = element_line(size = 0.5, color = "grey"), 
+        axis.line = element_line(size = 0.7, color = "black"), 
+        text = element_text(size = 14), axis.text.x = element_text(angle = 0, hjust = 1))
+        
 plot
